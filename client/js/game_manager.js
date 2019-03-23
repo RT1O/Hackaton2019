20d81b1fd162f5f4d3f05a84a5d187883b5b0c4b
Toms Zvirbulis <tomszvirbulis3@gmail.com>
2019-03-23 19:13:36 +0200

    Pievienoju viktorīnas grūtības ģenerāciju.

--- client/js/game_manager.js
+++ client/js/game_manager.js
@@ -1,26 +1,30 @@
 function getMap(coordinates, zoom, token) {
   const map = L.map('game-map').setView(coordinates, zoom);
   L.tileLayer('https://api.mapbox.com/styles/v1/rt1o/cjtijj0di2p881fp5d8n8gs27/tiles/{z}/{x}/{y}?access_token=' + token, {
-    minZoom: zoom
+    minZoom: zoom - 1
   }).addTo(map);
   return map;
 }
 
 function setGeoJson(map, data) {
   let geoJson;
+
   const colors = [
-    'rgb(0, 49, 66)',
-    'rgb(201, 209, 0)',
-    'rgb(214, 89, 167)'
+    'rgb(200, 200, 200)',
+    'rgb(117, 206, 135)',
+    'rgb(255, 165, 0)',
+    'rgb(196, 57, 58)'
   ];
 
-  function getColor(diff) {
-    return colors[diff % colors.length];
+  function getColor(properties) {
+    if (properties.completed)
+      return 'rgb(120, 120, 120)';
+    return colors[properties.diff % colors.length];
   }
 
   function style(feature) {
     return {
-      fillColor: getColor(feature.properties.diff),
+      fillColor: getColor(feature.properties),
       fillOpacity: 1.0,
       weight: 2,
       color: 'rgb(255, 255, 255)'
@@ -80,8 +84,50 @@ function setGeoJson(map, data) {
   return geoJson;
 }
 
-const map = getMap([56.946285, 24.105078], 7, 'pk.eyJ1IjoicnQxbyIsImEiOiJjanRoN2Rwb24wZm1nM3lwNjRjOWlmaGlkIn0.KQ0vUQXa5yDTCq9QF7siPA');
-const geoJson = setGeoJson(map, novadi.features.map((f) => {
-  f.properties.diff = Math.floor(Math.random() * 3);
-  return f;
-}));
+const map = getMap([56.946285, 24.105078], 7,
+  'pk.eyJ1IjoicnQxbyIsImEiOiJjanRoN2Rwb24wZm1nM3lwNjRjOWlmaGlkIn0.KQ0vUQXa5yDTCq9QF7siPA');
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
+function generateDifficulty(values) {
+  const set = [0, 0, 0];
+  const final = shuffle(novadi.features.map((f) => {
+    f.properties.diff = 0;
+    f.properties.completed = false;
+    return f;
+  }));
+
+  for (var x = 0; x < values.length; x++) {
+    while (set[x] < values[x]) {
+      for (var i = 0; i < final.length; i++) {
+        if (final[i].properties.diff > 0)
+          continue;
+        if (Math.floor(Math.random() * 100) > 80) {
+          if (set[x] >= values[x])
+            break;
+          final[i].properties.diff = (x + 1);
+          set[x] += 1;
+        }
+      }
+    }
+  }
+
+  for (var i = 0; i < final.length; i++) {
+    if (final[i].properties.diff > 0) {
+      final[i].properties.completed = (Math.floor(Math.random() * 100) > 80 ? true : false);
+    }
+  }
+
+  return final;
+}
+
+const geoJson = setGeoJson(map, generateDifficulty([15, 10, 5]));
