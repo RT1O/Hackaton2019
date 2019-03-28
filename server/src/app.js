56d7266e4b4afcc71e827190b9be28ba492537f4
Toms Zvirbulis <tomszvirbulis3@gmail.com>
2019-03-28 13:10:34 +0200

    Izlaboju Cross-Origin

--- server/src/app.js
+++ server/src/app.js
@@ -35,14 +35,11 @@ fs.readdirSync(__dirname + '/data')
 console.log('Loaded ' + csvAmt + ' .csv(s)');
 
 app.use(bodyParser.json());
-
-/*
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-*/
 
 app.get('/', (req, res) => {
   res.json(parsedCsv);
